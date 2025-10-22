210c9b213a22fd04a5248a17a017e236a617fc1a
Yuzt-zju <yuzt@zju.edu.cn>
2025-10-22 07:20:17 +0000

    update readme

--- step1.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python -m src.inference experiment=sem_embeds_inference_flat data_dir=data/amazon_data/beauty
-CUDA_VISIBLE_DEVICES=0 python -m src.inference experiment=sem_embeds_inference_flat data_dir=data/amazon_data/toys
-CUDA_VISIBLE_DEVICES=0 python -m src.inference experiment=sem_embeds_inference_flat data_dir=data/amazon_data/sports
--- train_gr.sh
+++ train_gr.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# 配置
 datasets=("beauty" "sports" "toys")
 gpus=("1" "1" "1")
 cpu_ranges=("11-20" "11-20" "11-20")
-seeds=(999 1024 2025)  # 添加 seed 列表
+seeds=(999 1024 2025)  
 
 experiment="tiger_train_flat"
 num_hierarchies=4
 mode=rkmeans
 
-# 顺序遍历每个数据集
 for i in "${!datasets[@]}"; do
     dataset="${datasets[$i]}"
     gpu="${gpus[$i]}"
     cpu_range="${cpu_ranges[$i]}"
 
-    # 对每个 seed 运行一次
+
     for seed in "${seeds[@]}"; do
         echo "Running dataset: $dataset on GPU $gpu, CPU cores $cpu_range, seed=$seed"
 
